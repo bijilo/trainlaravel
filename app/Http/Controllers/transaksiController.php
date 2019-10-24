8a72ffe4dce2f46dadfefffc6f6778b98808c3cb
bijilo <lbmineralsource@gmail.com>
2019-10-24 11:41:51 +0700

    menambahkan function index

--- app/Http/Controllers/transaksiController.php
+++ app/Http/Controllers/transaksiController.php
@@ -8,6 +8,10 @@ use App\transaksiModel;
 class transaksiController extends Controller
 {
     //
+    public function index(){
+        return "halu";
+    }
+
     public function store(){
         $data = new transaksiModel;
         return \Response::json($data->getData(), 200);
